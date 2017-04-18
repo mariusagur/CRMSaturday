40dc4bfabcfe4d90751bcbfec22e40aadecc7ac4
Marius Agur Pedersen <mariusap@outlook.com>
2017-04-18 13:37:25 +0200

    Added base solution, presentation and solution for workflows and plugins

---
+++ Zurich2017/IntegratingWithAzure/Dyn365PluginsAndWorkflows/SendRelayMessage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyn365PluginsAndWorkflows
+{
+    public class GetAllServiceEndpoints : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory =
+                (IOrganizationServiceFactory) serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+        }
+    }
+}
