04974afeee0a0bb75c30cd8b662094213d010aae
mariusagur <mariusap@gmail.com>
2017-04-23 16:17:16 +0200

    Added workflow to push context to service endpoint, added new relay app to debug two-way relay. Deleted old plugin draft which will not be used.

---
+++ Zurich2017/IntegratingWithAzure/Dyn365PluginsAndWorkflows/Workflows/PassContextToServiceEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Dyn365PluginsAndWorkflows.Workflows
+{
+    public class PassContextToServiceEndpoint : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+            var service = context.GetExtension<IServiceEndpointNotificationService>();
+            var response = service.Execute(ServiceEndpoint.Get(context), workflowContext);
+            ReturnValue.Set(context, response);
+        }
+
+        [Input("ServiceEndpoint")]
+        [ReferenceTarget("serviceendpoint")]
+        [RequiredArgument]
+        public InArgument<EntityReference> ServiceEndpoint { get; set; }
+
+        [Output("Service Return Value")]
+        public OutArgument<string> ReturnValue { get; set; }
+    }
+}
---
+++ Zurich2017/IntegratingWithAzure/RelayService/TwoWayRelay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.ServiceBus;
+using Microsoft.Xrm.Sdk;
+
+namespace RelayService
+{
+    internal class TwoWayRelay
+    {
+        public static void Main(string[] args)
+        {
+            var sh = new ServiceHost(typeof(RemoteService));
+            var tcpEndpointBehavior = new TransportClientEndpointBehavior();
+
+            var test = ConfigurationManager.AppSettings["SBusEndpoint"];
+            sh.AddServiceEndpoint(
+                    typeof(RemoteService).GetInterfaces().First(),
+                    new WS2007HttpRelayBinding(),
+                    ConfigurationManager.AppSettings["SBusEndpoint"]
+                )
+                .Behaviors.Add(tcpEndpointBehavior);
+            sh.Open();
+            Console.WriteLine("TwoWay-listener, press ENTER to close");
+            Console.ReadLine();
+
+            sh.Close();
+        }
+    }
+
+    [ServiceBehavior]
+    public class RemoteService : ITwoWayServiceEndpointPlugin
+    {
+        public string Execute(RemoteExecutionContext c)
+        {
+            Console.WriteLine(
+                $"Entity: {c.PrimaryEntityName}, id: {c.PrimaryEntityId}");
+            return "Message processed in two-way listener";
+        }
+    }
+}
